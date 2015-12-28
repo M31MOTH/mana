57857cd8a7a9ca9e857e9362d811ca723b1348e6
s0lst1c3 <solstice@member.fsf.org>
2015-12-28 00:53:35 -0500

    Fixed bug with net_creds plugin

--- plugins/net_creds.py
+++ plugins/net_creds.py
@@ -1,13 +1,14 @@
 
 from core import iptables
 from plugin_deps import net_creds
+from plugin import Plugin
 
 import sys, getopt, logging, traceback, string, os
 
 class Net_creds(Plugin):
 
-    name = 'net_creds'
-    optname = 'net-creds'
+    name = 'netcreds'
+    optname = 'netcreds'
     desc = 'Netcreds.'
     sleep_time = 2
 
@@ -35,6 +36,6 @@ class Net_creds(Plugin):
     @staticmethod
     def _start(configs):
 
-        net_creds.run(interface=configs['log_file'],
+        net_creds.run(interface=configs['interface'],
                     filterip=configs['filterip'],
                     pcap=None)
